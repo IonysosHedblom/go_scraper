560531e0b83707fbb1c01be4c88d4667059285de
Ionysos Hedblom <ionysoshedblom@gmail.com>
2021-10-26 23:33:06 +0200

    removed unused comments

--- cmd/main.go
+++ cmd/main.go
@@ -6,25 +6,10 @@ import (
 	server "github.com/ionysoshedblom/go_scraper/internal/framework/http"
 )
 
-// var baseUrl string = "https://www.ica.se/Templates/ajaxresponse.aspx?ajaxFunction=RecipeListMdsa&mdsarowentityid=&num=16&query=pasta&sortbymetadata=Relevance&id=12&_hour=7"
-
 func main() {
 	scraper := scraper.New()
 	applicationAPI := api.NewApplication(scraper)
 	
 	httpServer := server.NewServer(applicationAPI)
 	httpServer.Run()
-	// src, err := applicationAPI.GetSource(baseUrl)
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// htmlSrc, err := applicationAPI.HandleSource(src)
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// fmt.Println(htmlSrc)
 }
