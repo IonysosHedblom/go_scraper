b3cead33196b090de3811054ebb3390abfe5a269
Ionysos Hedblom <ionysoshedblom@gmail.com>
2021-11-03 07:39:47 +0100

    attemp to scrape for images

--- internal/application/scraper/scraper.go
+++ internal/application/scraper/scraper.go
@@ -16,7 +16,7 @@ func New() *Scraper {
 func (s Scraper) HandleSource(n *html.Node) ([]entity.Recipe, error) {
 	var titles []*bytes.Buffer
 	var desc []*bytes.Buffer
-	// var imageUrls []*bytes.Buffer
+	var imageUrls []*bytes.Buffer
 	var visitNode func(*html.Node)
 
 	visitNode = func(n *html.Node) {
@@ -32,11 +32,11 @@ func (s Scraper) HandleSource(n *html.Node) ([]entity.Recipe, error) {
 			desc = append(desc, dBuf)
 		}
 
-		// if n.Type == html.ElementNode && n.Parent.Data == "noscript" && n.Data == "img" {
-		// 	iBuf := &bytes.Buffer{}
-		// 	writeNodeContentToBuffer(n, iBuf)
-		// 	imageUrls = append(imageUrls, iBuf)
-		// }
+		if n.Type == html.ElementNode && n.Parent.Data == "noscript" && n.Data == "img" {
+			iBuf := &bytes.Buffer{}
+			writeNodeContentToBuffer(n, iBuf)
+			imageUrls = append(imageUrls, iBuf)
+		}
 
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			visitNode(c)
@@ -67,9 +67,9 @@ func writeNodeContentToBuffer(n *html.Node, buf *bytes.Buffer) {
 		buf.WriteString(n.Data)
 	}
 
-	// if n.Type == html.ElementNode {
-	// 	buf.WriteString(n.Data)
-	// }
+	if n.Type == html.ElementNode {
+		buf.WriteString(n.Data)
+	}
 
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		writeNodeContentToBuffer(c, buf)
