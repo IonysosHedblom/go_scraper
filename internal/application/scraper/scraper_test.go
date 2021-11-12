70102a42378d3eb4bdf895860deac7534f32c351
Ionysos Hedblom <ionysoshedblom@gmail.com>
2021-11-12 09:58:28 +0100

    added tests for existsinslice method

--- internal/application/scraper/scraper_test.go
+++ internal/application/scraper/scraper_test.go
@@ -1,1 +1,16 @@
 package scraper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExistsInSlice(t *testing.T) {
+	testSlice := []string{"test", "test1"}
+	testValue := "test3"
+	expected := true
+	actual := existsInSlice(testSlice, testValue)
+
+	assert.Equal(t, expected, actual)
+}
