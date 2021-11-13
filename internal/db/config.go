31e7093f24e5758f233169caa47412ea6b3d3494
Ionysos Hedblom <ionysoshedblom@gmail.com>
2021-11-13 13:54:33 +0100

    disabled .env and loading from regular file while in development, something wrong with debugger and env vars

--- internal/db/config.go
+++ internal/db/config.go
@@ -2,23 +2,19 @@ package db
 
 import (
 	"fmt"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
-func getEnv(key string) string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Println(err.Error())
-		log.Fatalf("Error loading .env file")
-	}
+// func getEnv(key string) string {
+// 	err := godotenv.Load(".env")
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		log.Fatalf("Error loading .env file")
+// 	}
 
-	return os.Getenv(key)
-}
+// 	return os.Getenv(key)
+// }
 
 func CreateDbDSN() string {
-	dsn := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", getEnv("DB_USER"), getEnv("DB_PASSWORD"), getEnv("DB_NAME"))
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
 	return dsn
 }
